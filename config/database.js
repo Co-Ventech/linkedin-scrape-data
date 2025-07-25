87b8aa4371494aeac61a5c804ec4c3a52017181f
muhammadehmed123 <ahmadsertaj876@gmail.com>
2025-07-25 18:27:05 +0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,15 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   const mongoURI = process.env.MONGO_URL;
-  console.log("🔍 MONGO_URL =", mongoURI); // Add this line
-
-  if (!mongoURI) {
-    console.error('❌ MONGO_URL is missing from environment variables!');
-    process.exit(1);
-  }
-
+  console.log("here is the mongo url :", mongoURI);
+  
   try {
-    await mongoose.connect(mongoURI, {});
+    await mongoose.connect(mongoURI, {
+    });
     console.log('✅ MongoDB connected');
   } catch (err) {
     console.error('❌ MongoDB connection error:', err);
